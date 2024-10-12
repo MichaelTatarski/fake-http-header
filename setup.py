c26f399e89ee3b0471fd79468c958b8c67f65301
Michael Tatarski <michael.tatarski@siemens.com>
2024-10-12 13:03:42 +0200

    Update version to 0.2 in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="fake-http-header",
-    version="0.1.1",
+    version="0.2",
     description="Generates random request fields for a http header",
     long_description=README,
     long_description_content_type="text/markdown",
